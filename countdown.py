d92f717072377f37986d9dba8c16068645eb9eb9
kevv10 <159073492+kevv10@users.noreply.github.com>
2024-04-07 22:57:12 -0500

    Update countdown.py
    Timer now plays audio at 15 seconds. Had to make each second a little longer to match up the audio

--- countdown.py
+++ countdown.py
@@ -1,30 +1,36 @@
 import tkinter as tk
+import threading
+import time
 from PIL import Image, ImageTk
+from playsound import playsound
 
 def countdown_start():
     def update_countdown():
         nonlocal countdown_value
+        if(countdown_value == 15):
+            threading.Thread(target=playsound, args=("Track01.mp3",), daemon=True).start()
         if countdown_value > 0:
             # Update label with the current countdown value
             label.config(text=str(countdown_value))
             countdown_value -= 1
         elif countdown_value == 0:
             # Change the text to "Go!" before closing
+            time.sleep(1)
             label.config(text="Go!")
             countdown_value -= 1
         else:
             # Close the window after showing "Go!"
             root.destroy()
-        root.after(1000, update_countdown)  # Schedule the function to be called after 1s
+        root.after(1035, update_countdown)  # Schedule the function to be called after 1s
 
     root = tk.Tk()
     root.attributes("-fullscreen", True)
 
     # Initialize countdown value
-    countdown_value = 3
+    countdown_value = 30
 
     # Use a label to display countdown
-    label = tk.Label(root, text="", font=("Helvetica", 90), fg='orange',bg='black')
+    label = tk.Label(root, text="", font=("Helvetica", 90), fg='orange', bg='black')
     label.pack(expand=True, fill=tk.BOTH)
 
     # Start the countdown
